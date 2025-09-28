784e2580882eed7192b8bc92468029a981d102c9
sahaGramovich <sahamaks2005@gmail.com>
2025-09-28 13:40:49 +0300

    переделал 11 и 2 задание

--- laba1/task11.py
+++ laba1/task11.py
@@ -1,35 +1,57 @@
+month = int(input("Введите месяц: "))
+day = int(input("Введите число: "))
 
-print("Введите дату вашего рождения:")
-day = int(input("День (число): "))
-month = int(input("Месяц (число от 1 до 12): "))
-
+print("Ваша дата: " + str(month) + "." + str(day))
 
 if (month == 12 and day >= 22) or (month == 1 and day <= 19):
+
     sign = "Козерог"
+
 elif (month == 1 and day >= 20) or (month == 2 and day <= 18):
+
     sign = "Водолей"
+
 elif (month == 2 and day >= 19) or (month == 3 and day <= 20):
+
     sign = "Рыбы"
+
 elif (month == 3 and day >= 21) or (month == 4 and day <= 19):
+
     sign = "Овен"
+
 elif (month == 4 and day >= 20) or (month == 5 and day <= 20):
+
     sign = "Телец"
+
 elif (month == 5 and day >= 21) or (month == 6 and day <= 20):
+
     sign = "Близнецы"
+
 elif (month == 6 and day >= 21) or (month == 7 and day <= 22):
-    sign = "Рак"
+
+    sign = "Paк"
+
 elif (month == 7 and day >= 23) or (month == 8 and day <= 22):
-    sign = "Лев"
+
+    sign = "Лeв"
+
 elif (month == 8 and day >= 23) or (month == 9 and day <= 22):
+
     sign = "Дева"
+
 elif (month == 9 and day >= 23) or (month == 10 and day <= 22):
-    sign = "Весы"
+
+    sign = "Becы" 
+
 elif (month == 10 and day >= 23) or (month == 11 and day <= 21):
+
     sign = "Скорпион"
+
 elif (month == 11 and day >= 22) or (month == 12 and day <= 21):
+
     sign = "Стрелец"
+
 else:
     sign = "Неизвестно (проверьте правильность ввода)"
-
 
 print(f"\nВаш знак зодиака: {sign}")
--- laba1/task2.py
+++ laba1/task2.py
@@ -1,7 +1,15 @@
 
 text = input("Введите строку: ")
 
-vowels = "aeiouAEIOU"
-result = ''.join([char for char in text if char not in vowels])
+text = text.replace('a', '')
+text = text.replace('e', '')
+text = text.replace('i', '')
+text = text.replace('o', '')
+text = text.replace('u', '')
+text = text.replace('A', '')
+text = text.replace('E', '')
+text = text.replace('I', '')
+text = text.replace('O', '')
+text = text.replace('U', '')
 
-print("Результат:", result)
+print(text)
